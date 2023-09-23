b74f681ba504228dd7a98cd3324417a8cd6c4a12
Jonathann Jefferson <jonathannjefferson10@gmail.com>
2023-09-22 22:01:19 -0300

    Initial commit

---
+++ src/test/java/DesafioApi/DesafioAPI/DesafioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package DesafioApi.DesafioAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
