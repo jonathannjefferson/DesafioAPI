ce6fc07f2edcee17556463fc89ddd0703ff179de
Jonathann Jefferson <jonathannjefferson10@gmail.com>
2023-09-27 00:45:09 -0300

    feat(dev): criacao do user

---
+++ src/main/java/DesafioApi/domain/model/User.java
@@ -0,0 +1,46 @@
+package DesafioApi.domain.model;
+
+import DesafioApi.domain.model.Car;
+import jakarta.persistence.*;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_Name")
+    private String lastName;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @NotBlank
+    @Column(name = "birthday")
+    private Date birthday;
+
+    @Column(name = "login",unique = true)
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Car> cars;
+
+}
