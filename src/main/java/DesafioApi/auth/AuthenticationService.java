80a9783feeea5d343da7b6d2f5f0aeda09909727
Jonathann Jefferson <jonathannjefferson10@gmail.com>
2023-09-28 23:48:57 -0300

    feat(dev): criacao da classe AuthenticationService para nível de registro e autenticacao referente ao token

---
+++ src/main/java/DesafioApi/auth/AuthenticationService.java
@@ -0,0 +1,53 @@
+package DesafioApi.auth;
+
+import DesafioApi.Repository.UserRepository;
+import DesafioApi.config.JwtService;
+import DesafioApi.domain.model.Role;
+import DesafioApi.domain.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .login(request.getLogin())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                        request.getLogin(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByLogin(request.getLogin())
+                .orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+}
