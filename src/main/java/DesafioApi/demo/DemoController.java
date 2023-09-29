ce7f9a8edbf5db21c39d945cdcbc77914668759b
Jonathann Jefferson <jonathannjefferson10@gmail.com>
2023-09-28 23:49:06 -0300

    feat(dev): criacao da classe DemoController apenas para teste.

---
+++ src/main/java/DesafioApi/demo/DemoController.java
@@ -0,0 +1,17 @@
+package DesafioApi.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/teste")
+public class DemoController {
+
+
+    @GetMapping
+    public ResponseEntity<String> sayhello(){
+        return ResponseEntity.ok("Hello World");
+    }
+}
