a56232421a5aa9a027a7d3d5bd02a2a31c9fd3ac
Jonathann Jefferson <jonathannjefferson10@gmail.com>
2023-09-28 23:52:50 -0300

    chore(dev): ajustes na classe user

--- src/main/java/DesafioApi/domain/model/User.java
+++ src/main/java/DesafioApi/domain/model/User.java
@@ -2,17 +2,21 @@ package DesafioApi.domain.model;
 
 import jakarta.persistence.*;
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
-@Getter
-@Setter
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,7 +30,6 @@ public class User {
     @Column(name = "email", unique = true)
     private String email;
 
-    @NotBlank
     @Column(name = "birthday")
     private Date birthday;
 
@@ -42,4 +45,42 @@ public class User {
     @OneToMany(cascade = CascadeType.ALL)
     private List<Car> cars;
 
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
